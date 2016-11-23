e25d84390274b9e30b9fec0b2066a24daf5cc748
my-PC\my <01032372763@naver.com>
2016-11-23 15:17:36 +0900

    String Tokenizer opcode 한 줄씩 읽고 Tokenizer로 끊어서 명령어 받기

--- SIC/SIC/function.c
+++ SIC/SIC/function.c
@@ -1,5 +1,26 @@
 ﻿#include "function.h"
 
+#define MAX 100
+
+FILE *inFile;	//opcode.s
+FILE *opcFile;	//opcode
+
+typedef struct {
+	char lable[7];
+	char inst[7];
+	char symbol[7];
+	int length;
+}INPUT;
+
+typedef struct {
+	char inst[7];
+	char code[7];
+	int length;
+}OPCODE;
+
+INPUT intab[MAX];
+OPCODE opctab[MAX];
+
 //파일 읽는 함수
 void fileRead() {
 	
@@ -19,3 +40,84 @@ void SkipSpace() {
 		Index++; // 버퍼 인덱스
 }
 //gggggggg
+char strtok() {
+	inFile = fopen("./opcode.s", "r");
+	opcFile = fopen("./opcode", "r");
+	int i = 0;
+	int j = 0;
+	//<<----------------------------------input.txt open--------------------------------->>
+
+	memset(intab, 0, sizeof(intab));
+	char inTemp[255] = "\0";
+	char *inLine;
+	char *inToken;
+	char inSeps[] = "\n\t";
+	intab[0].length = 0;
+	if (inFile != NULL) {									
+		while (!feof(inFile)) {						//inFile의 끝까지
+			inLine = fgets(inTemp, sizeof(inTemp), inFile);		// 한 줄씩 읽어들이기
+			i = 0;
+			inToken = strtok(inLine, inSeps);
+			while (inToken != NULL) {				//inToken이 Not NULL때 까지
+				if (i == 0) {
+					strcpy(intab[intab[0].length].lable, inToken);	//i가 0일 때 inToken을 intab의 문자열을 label에 넣는다.
+				}
+				else if (i == 1) {
+					strcpy(intab[intab[0].length].inst, inToken);   //label을 넣은 후 i++ 후에 다음 Token을 intab의 inst에 넣는다.
+				}
+				else {
+					strcpy(intab[intab[0].length].symbol, inToken);  //나머지 Token을 intab의 symbol에 저장.
+				}
+				i++;
+				inToken = strtok(NULL, inSeps);
+			}
+			intab[0].length++;
+		}
+		/*
+		for(j=0; j<intab[0].length-1; j++)
+		printf("[%s] [%s] [%s]\n",intab[j].lable, intab[j].inst, intab[j].symbol);
+		*/
+	}
+	else {
+		printf("no file !!");
+	}
+
+	//<<----------------------------------opcode.txt open--------------------------------->>
+
+	memset(opctab, 0, sizeof(opctab));
+	char opcTemp[255] = "\0";
+	char *opcLine;
+	char *opcToken;
+	char opcSeps[] = "\n\t";
+	opctab[0].length = 0;
+
+	if (opcFile != NULL) {
+		while (!feof(opcFile)) {
+			opcLine = fgets(opcTemp, sizeof(opcTemp), opcFile);
+			i = 0;
+			opcToken = strtok(opcLine, opcSeps);
+			while (opcToken != NULL) {
+				if (i == 0) {
+					strcpy(opctab[opctab[0].length].inst, opcToken);	//첫 번째 등장하는 Token을 inst에 저장.
+				}
+				else {
+					strcpy(opctab[opctab[0].length].code, opcToken);	//두 번째 등장하는 Token을 code에 저장
+				}
+				i++;
+				opcToken = strtok(NULL, opcSeps);
+			}
+			opctab[0].length++;
+		}
+		/*
+		for(j=0; j<opctab[0].length-1; j++)
+		printf("[%s] [%s]\n", opctab[j].inst, opctab[j].code);
+		*/
+	}
+	else {
+		printf("no file !!");
+	}
+
+	//file close
+	fclose(inFile);
+	fclose(opcFile);
+}
