548dd42509f07031d22e16dfff02e421af864d31
moomPC\문성현 <mc08188@gmail.com>
2016-11-24 23:51:37 +0900

    일단 지금 저장된 optable파일로 하면 나오는데 optable 바꾸면 안나올거 같아요

--- SIC/SIC/SIC_TEAM_FIVE.c
+++ SIC/SIC/SIC_TEAM_FIVE.c
@@ -24,13 +24,13 @@ struct operators {
 };
 
 /*struct operators instruction[26] = { //OPTAB
-	{ "ADD", "18" },{ "AND", "40" },{ "COMP", "28" },{ "DIV", "24" },
-	{ "J", "3C" },{ "JEQ", "30" },{ "JGT", "34" },{ "JLT", "38" },
-	{ "JSUB", "48" },{ "LDA", "00" },{ "LDCH", "50" },{ "LDL", "08" },
-	{ "LDX", "04" },{ "MUL", "20" },{ "OR", "44" },{ "RD", "D8" },
-	{ "RSUB", "4C" },{ "STA", "0C" },{ "STCH", "54" },{ "STL", "14" },
-	{ "STSW", "E8" },{ "STX", "10" },{ "SUB", "1C" },{ "TD", "E0" },
-	{ "TIX", "2C" },{ "WD", "DC" }
+{ "ADD", "18" },{ "AND", "40" },{ "COMP", "28" },{ "DIV", "24" },
+{ "J", "3C" },{ "JEQ", "30" },{ "JGT", "34" },{ "JLT", "38" },
+{ "JSUB", "48" },{ "LDA", "00" },{ "LDCH", "50" },{ "LDL", "08" },
+{ "LDX", "04" },{ "MUL", "20" },{ "OR", "44" },{ "RD", "D8" },
+{ "RSUB", "4C" },{ "STA", "0C" },{ "STCH", "54" },{ "STL", "14" },
+{ "STSW", "E8" },{ "STX", "10" },{ "SUB", "1C" },{ "TD", "E0" },
+{ "TIX", "2C" },{ "WD", "DC" }
 };*/
 
 typedef struct TABLE {
@@ -66,10 +66,10 @@ void loadTable() {           //파일에 저장된 OPTAB 열기
 	FILE* fp;
 	int i = 0;
 
-     fopen_s(&fp, "optable", "r");
+	fopen_s(&fp, "optable", "r");
 
 	while (!feof(fp)) {
-		fscanf_s(fp, "%s ", table[i].memory,MEMORY_MAX);
+		fscanf_s(fp, "%s ", table[i].memory, MEMORY_MAX);
 		fscanf_s(fp, "%s", table[i].opcode, OPCODE_MAX);
 		i++;
 	}
@@ -80,29 +80,29 @@ void loadTable() {           //파일에 저장된 OPTAB 열기
 int path1() {
 	FILE* fp;
 	char buffer[MAX];       //파일 opcode에 저장된 어셈블 소스를 버퍼 배열에 옮김
-	char* token;           
+	char* token;
 	int lineCount = 0;      //선언하므로써 어셈블 소스의 총길이를 알 수 있음
 	char* context = NULL;   //strtok_s 사용하기 위해 선언 strtok 사용하면 필요 없음, 버퍼에 남은 문자열
 	fopen_s(&fp, "opcode.s", "r");     //저장된 어셈블 소스 열기
 	while (fgets(buffer, MAX, fp) != NULL) {
 
 		if (buffer[0] != '\t') {                        //opcode에 저장된 이섬블 소스 tab으로 구분, label값이 tab이 아니라면
-			token = strtok_s(buffer, "\t\n",&context);  //문자열로 분할하여 토큰에 넣어줌
-			
-			strcpy_s(line[lineCount].label, 10,token);  //토큰값에 들어있는걸 line[lineCount].label에 복사함
-			token = strtok_s(NULL, "\t\n",&context);    //context에 남은 문자열이 없다면 null을 반환하고 있다면 남은 문자열 토큰에 넣어줌
+			token = strtok_s(buffer, "\t\n", &context);  //문자열로 분할하여 토큰에 넣어줌
+
+			strcpy_s(line[lineCount].label, 10, token);  //토큰값에 들어있는걸 line[lineCount].label에 복사함
+			token = strtok_s(NULL, "\t\n", &context);    //context에 남은 문자열이 없다면 null을 반환하고 있다면 남은 문자열 토큰에 넣어줌
 		}
 		else {                                         //원시문 label값이 없고 tab이 있다면
-			strcpy_s(line[lineCount].label,10, "");    //label 값이 없으므로 공백 넣어줌 
+			strcpy_s(line[lineCount].label, 10, "");    //label 값이 없으므로 공백 넣어줌 
 
-			token = strtok_s(buffer, "\t\n",&context); //label값이 없으므로 다음에 나올 memory값을 토큰에 넣어줌
+			token = strtok_s(buffer, "\t\n", &context); //label값이 없으므로 다음에 나올 memory값을 토큰에 넣어줌
 		}
 
-		strcpy_s(line[lineCount].memory,10, token);   //토큰값에 들어있는걸 line[lineCount].memory에 복사함
+		strcpy_s(line[lineCount].memory, 10, token);   //토큰값에 들어있는걸 line[lineCount].memory에 복사함
 
-		token = strtok_s(NULL, "\t\n",&context);
+		token = strtok_s(NULL, "\t\n", &context);
 
-		strcpy_s(line[lineCount].location,10, token);  //토큰값에 들어있는걸 line[lineCount].location에 복사함
+		strcpy_s(line[lineCount].location, 10, token);  //토큰값에 들어있는걸 line[lineCount].location에 복사함
 
 		printf("%s %s %s\n", line[lineCount].label, line[lineCount].memory, line[lineCount].location);
 
@@ -126,13 +126,13 @@ int cal(lines) {            //원시문 location값
 		hex /= 10;
 		radix *= 16;
 	}
-	
+
 	for (int i = 1; i < lines; i++) {
-			strcpy_s(label[labelCount].label,10, line[i].label);  //line[i].label에 있는 값을 label[labelCount].label 옮김
-			label[labelCount].location = decimal;                 //원시문 location 값에 위에서 변환한 10진수 삽입(처음 시작주소)
-			printf("%06x %s\n", label[labelCount].location, label[labelCount].label);
-			labelCount++;
-		
+		strcpy_s(label[labelCount].label, 10, line[i].label);  //line[i].label에 있는 값을 label[labelCount].label 옮김
+		label[labelCount].location = decimal;                 //원시문 location 값에 위에서 변환한 10진수 삽입(처음 시작주소)
+		printf("%06x %s\n", label[labelCount].location, label[labelCount].label);
+		labelCount++;
+
 		decimal += sizeCheck(i);                                  //location 값 증가시키기 위해 sizeCheck함수 호출
 	}
 
@@ -146,42 +146,138 @@ int sizeCheck(int i) {
 		int j;
 		int flag = 0;
 		int count = 0;
-
-		for (j = 0; j < strlen(line[i].location); j++) {          //원시문 location길이만큼 포문을 돔
-			if (flag)                                             //로케이션 값이 문자면 count 증가
+		int c = 0;
+		for (j = 0; j < strlen(line[i].location); j++) {  		//원시문 location길이만큼 포문을 돔
+																//if (flag)                                             //로케이션 값이 문자면 count 증가
+																//	count++;
+																//	if (!flag && line[i].location[j] == '\'')             //로케이션 값에 '가 나오면 flag 증가
+																//	flag = 1;
+																//else if (line[i].location[j] == '\'') 
+																//break;
+			if (line[i].location[j] != 'C' && line[i].location[j] != '\'') {
 				count++;
-			if (!flag && line[i].location[j] == '\'')             //로케이션 값에 '가 나오면 flag 증가
-				flag = 1;
-			//else if (line[i].location[j] == '\'') 
-				//break;
+			}
+
+
 		}
 		return count;
 	}
 	else if (strcmp(line[i].memory, "RESB") == 0) {              //원시문 메모리값이 RESB이면 RESB값만큼 리턴
-		return atoi(line[i].location) ;
+		return atoi(line[i].location);
 	}
 	else {
 		return WORD_SIZE;                                        //위에 경우가 전부 아니면 WORD_SIZE만 증가
 	}
 }
-void path2();
-struct oneline readline(FILE *, int);
-int lookup(char *);
-void insert(char *, int);
-int srchop(char *); //OPCODE를 위해 OPTAB를 검색, 리턴값: 검색된 OP의 배열의 첨자 + 1, 실패시 0
-int xtoi(char *);
-int srchoperand(char *s);
-char * file1[20];
-char * file2[20];
+int getOpcode(int index) {
+	for (int i = 0; i < MAX; i++) {
+		if (strcmp(table[i].memory, line[index].memory) == 0)
+			return i;
+	}
+}
+int getLocation(char symbol[]) {
+	for (int i = 0; i < MAX; i++) {
+		if (strcmp(symbol, label[i].label) == 0)
+			return label[i].location;
+	}
+}
+void path2(int lines, int totallines) {
+	int i = 0;
+	int hex = atoi(line[0].location);
+	int decimal = 0;
+	int radix = 1;
+	int decimalcount = 0;
+	while (hex != 0) {                                   //10진수 16진수로 바꾸는데 사용
+		decimal += (hex % 10) * radix;
+		hex /= 10;
+		radix *= 16;
+	}
+	int startLocation = decimal;
+
+	printf("\n");
+	printf("H %s %06x %06x\n", line[0].label, decimal, totallines - decimal);    //Header 레코드 출력
+	for (i = 1; i < lines; i++) {                                              //Text 레코드 길이를 구하기 위한 포문
+		if (strcmp(line[i].memory, "RESB") == 0 || strcmp(line[i].memory, "RESW") == 0)
+			break;
+		else if (strcmp(line[i].memory, "BYTE") == 0 && strcmp(line[i].location, "C'EOF'") == 0 && decimalcount < 30)
+			decimalcount += 3;
+		else if (strcmp(line[i].memory, "WORD") == 0 && decimalcount < 30)
+			decimalcount += 3;
+		else
+			decimalcount += 3;
+		if (decimalcount == 30)
+			break;
+		if (decimalcount > 30) {
+			decimalcount -= 3;
+			break;
+		}
+	}
+	printf("T %06x %02x ", decimal, decimalcount);      //Text 레코드 시작주소 길이 출력
+	decimal = 0;
+	decimalcount = 0;
+	for (i; i <= lines; i++) {                                                  //Text 레코드 길이를 구하기 위한 포문
+		if (strcmp(line[i + 1].memory, "RESB") == 0 || strcmp(line[i + 1].memory, "RESW") == 0)
+			break;
+		else if (strcmp(line[i + 1].memory, "BYTE") == 0 && strcmp(line[i + 1].location, "C'EOF'") == 0 && decimalcount < 30)
+			decimalcount += 3;
+		else if (strcmp(line[i + 1].memory, "WORD") == 0 && decimalcount < 30)
+			decimalcount += 3;
+		else
+			decimalcount += 3;
+		if (decimalcount == 30)
+			break;
+		if (decimalcount > 30) {
+			decimalcount -= 3;
+			break;
+		}
+	}
+	for (i = 1; i < lines; i++) {                                             //Text 레코드 길이와 목적코드 출력 위한 포문
+		if (strcmp(line[i].memory, "RESB") == 0 || strcmp(line[i].memory, "RESW") == 0)
+			break;
+		else if (strcmp(line[i].memory, "BYTE") == 0 && strcmp(line[i].location, "C'EOF'") == 0 && decimal<30) {
+			printf("454F46 ");
+			decimal += 3;
+		}
+		else if (strcmp(line[i].memory, "WORD") == 0 && decimal<30) {
+			int wordhex = atoi(line[i].location);
+
+			int worddecimal = 0;
+			int wordradix = 1;
+			while (wordhex != 0) {                                     //10진수 16진수 변환
+				worddecimal += (wordhex % 10) * wordradix;
+
+				wordhex /= 10;
+				wordradix *= 16;
+			}
+			printf("%06x ", worddecimal);
+			decimal += 3;
+		}
+		else if (decimal<30) {
+			int opcodeIndex = getOpcode(i);                              //opcode 찾는 함수호출
+			int decimalAddress = getLocation(line[i].location);          //원시문에 적힌 location값 찾는 함수 호출 ex(RETADR = 1027)
+
+			printf("%s%x ", table[opcodeIndex].opcode, decimalAddress);   //목적코드 출력
+			decimal += 3;
+		}
+		if (decimal >= 30) {
+			printf("\n");
+			printf("T %06x %02x ", label[i].location, decimalcount);      //길이가 30넘어가면 점프? 해주고 다시 Text 레코드 시작주소 길이 출력
+			decimal = 0;
+		}
+	}
+	printf("\n");
+	printf("E %06x\n", startLocation);                                 //end 레코드 출력
+
+}
 int main() {
 	int lines;
-	int num=1000%10;
+	int totallines;
 	loadTable();
-	
+
 	lines = path1();
 
-	cal(lines);
-	
+	totallines = cal(lines);
+
+	path2(lines, totallines);
+
 }
-//ㅎㅎ 난똥이야!
-// ㅎㅎ 난 똥이다!
